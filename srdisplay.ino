be0331c0e78fab4a16f141f7ac2d49e510848be1
Ken Yap <retiredfeline@outlook.com>
2021-09-19 12:09:38 +1000

    #define options for 24 hour clock display, and inverted polarity for sinking current through LEDs

--- srdisplay.ino
+++ srdisplay.ino
@@ -6,6 +6,9 @@
 #define CLOCK   3
 #define LOAD    4
 
+#undef	COUNTER
+#define	LOWON
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(DATA, OUTPUT);
@@ -18,7 +21,11 @@ void setup() {
 
 void writebyte(unsigned int i) {
   for (byte mask = 0x80; mask != 0; mask >>= 1) {
+#ifdef	LOWON
+    digitalWrite(DATA, i & mask ? LOW : HIGH);
+#else
     digitalWrite(DATA, i & mask ? HIGH : LOW);
+#endif	// LOWON
     delayMicroseconds(10);
     digitalWrite(CLOCK, HIGH);
     delayMicroseconds(10);
@@ -42,8 +49,17 @@ void write2bytes(unsigned int i, unsigned int j) {
 
 void loop() {
   // put your main code here, to run repeatedly:
+#ifdef	COUNTER
   for (unsigned int i = 0; i < 256; i++) {
     write2bytes(i, i);
     delay(250);
   }
+#else
+  for (unsigned char hour = 0; hour < 24; hour++) {
+    for (unsigned char minute = 0; minute < 60; minute++) {
+       write2bytes(minute, hour);
+       delay(250);
+    }
+  }
+#endif	// COUNTER
 }
